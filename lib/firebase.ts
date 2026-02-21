fc743faf59a08f44725fb17f601263f9e760d069
euro <tibet.c@ku.th>
2026-02-22 01:03:33 +0700

    fix env

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore, Firestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAutiLS8dxNiRHQrx0BUGLvtoeMsKPZZa4",
-  authDomain: "myeng-fd4fc.firebaseapp.com",
-  projectId: "myeng-fd4fc",
-  storageBucket: "myeng-fd4fc.firebasestorage.app",
-  messagingSenderId: "1063834456789",
-  appId: "1:1063834456789:web:d82c5b1ae5729b17cac85c",
-  measurementId: "G-HM1VM80EZY"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
